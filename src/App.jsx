05ed0fc2fa6b98648ca6da74ab21cdd94214018c
Tomás Farías <83795131+SimuladorDeFarm@users.noreply.github.com>
2023-10-17 13:49:14 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import miImagen from './juan carlos bodoque.jpg'
 
 function App() {
   const backgroundImage = {
@@ -10,13 +11,40 @@ function App() {
     minHeight: '100vh',
   };
 
+
+  
   return (
-    <body className="App" style={backgroundImage}>
+
+    <div className="App" style={backgroundImage}>
+
+     
+        <div className='cabecera'>
+          
+          <div className='img-cabecera'>
+            <img src={miImagen} alt="Mi Imagen" className="mi-imagen" />
+          </div>
+          
+          <div className='titulo'>
+            <h1>31 Minutos</h1>
+          </div>
+
+        </div>
+         
+    
+      
+      <div className='BarraLinks'>
+        <button className='button1'>
+          Button1
+          
+        </button>
+ 
+      </div>
+
       <div className="Right_div">
         <h1>Tu Título</h1>
         <p>este es mi parrafo que estoy escribiendo en este momento</p>
       </div>
-    </body>
+    </div>
   );
 }
 
